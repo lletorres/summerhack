7a3ed2629c979ff905131a6496f446fcd2c1b460
Leandro Torres <paisark@MacBook-Pro-de-Leandro.local>
2024-11-05 03:47:55 -0300

    Mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ComponenteNuevo from "./ComponenteNuevo";
+
+function App() {
+  return (
+    <>
+      <h1>Hola mundo desde react</h1>
+      <p>te queremos vite y react</p>
+      <ComponenteNuevo> </ComponenteNuevo>
+    </>
+  );
+}
+
+export default App;
---
+++ src/ComponenteNuevo.jsx
@@ -0,0 +1,5 @@
+function ComponenteNuevo() {
+  return <h1>Componente Nuevooo</h1>;
+}
+
+export default ComponenteNuevo;
